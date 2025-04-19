1a992335a72543918acb605d1e7a1baaf70efa1b
Dlurak <84224239+Dlurak@users.noreply.github.com>
2025-04-19 02:15:28 +0200

    Add a height and width argument

---
+++ src/cli.rs
@@ -0,0 +1,95 @@
+use crate::flag::Flag;
+use clap::Parser;
+use std::{
+    num::{IntErrorKind, NonZero},
+    str::FromStr,
+};
+
+#[derive(Parser)]
+pub struct Cli {
+    #[arg(value_parser = parse_flag)]
+    pub flag: Flag,
+    #[arg(short, long, value_parser = parse_width)]
+    pub width: Option<Size>,
+    #[arg(long, value_parser = parse_width)]
+    pub height: Option<Size>,
+}
+
+fn parse_flag(s: &str) -> Result<Flag, String> {
+    Flag::from_str(s).map_err(|_| format!("\"{s}\" isn't a recognized flag, if it should be available please consider opening a pr :)"))
+}
+
+#[derive(Clone)]
+pub enum Size {
+    AbsoluteChars(NonZero<usize>),
+    Percentage(NonZero<u8>),
+}
+
+impl Size {
+    pub fn absolute_width(&self) -> Option<NonZero<usize>> {
+        match self {
+            Self::AbsoluteChars(c) => Some(*c),
+            Self::Percentage(perc) => {
+                let (w, _) = term_size::dimensions_stdout()?;
+                let w = w as f32;
+                let perc = perc.get() as f32;
+                NonZero::new((w / 100.0 * perc) as usize)
+            }
+        }
+    }
+    pub fn absolute_height(&self) -> Option<NonZero<usize>> {
+        match self {
+            Self::AbsoluteChars(c) => Some(*c),
+            Self::Percentage(perc) => {
+                let (_, h) = term_size::dimensions_stdout()?;
+                let h = h as f32;
+                let perc = perc.get() as f32;
+                NonZero::new((h / 100.0 * perc) as usize)
+            }
+        }
+    }
+}
+
+fn parse_width(s: &str) -> Result<Size, String> {
+    match s.parse() {
+        Ok(num) => return Ok(Size::AbsoluteChars(num)),
+        Err(err) => match err.kind() {
+            IntErrorKind::Zero => return Err(String::from("The width must be greater than zero")),
+            IntErrorKind::Empty => return Err(String::from("The width must not be empty")),
+            IntErrorKind::NegOverflow | IntErrorKind::PosOverflow => {
+                return Err(format!(
+                    "Your provided width doesn't fit into the integer (1-{})",
+                    usize::MAX
+                ))
+            }
+            _ => {}
+        },
+    }
+
+    let mut s = String::from(s);
+    match s.pop() {
+        Some('%') => {}
+        Some(_) => return Err(String::from("Relative units must end with a '%'")),
+        None => unreachable!("We already remove empty ones above"),
+    }
+
+    match s.parse::<NonZero<u8>>() {
+        Ok(width) => {
+            if width.get() > 100 {
+                Err(String::from("The width must not exceed 100%"))
+            } else {
+                Ok(Size::Percentage(width))
+            }
+        }
+        Err(err) => {
+            let msg = match err.kind() {
+                IntErrorKind::Zero => "The width must be at least 1%",
+                IntErrorKind::Empty | IntErrorKind::InvalidDigit => "Please provide a number",
+                IntErrorKind::NegOverflow => "The width must be at least 1%",
+                IntErrorKind::PosOverflow => "The width must not exceed 100%",
+                _ => todo!("This is a bug, please create a GitHub issue to report it!"),
+            };
+            Err(msg.into())
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,23 @@ use jiman::{
     color::{AnsiCode, RESET},
 };
 
-const WIDTH: usize = 60;
-const HEIGHT: usize = 14;
-
 fn main() {
     let cli = Cli::parse();
+    let width = cli
+        .width
+        .and_then(|w| w.absolute_width())
+        .map_or(60, |v| v.get());
+    let height = cli
+        .height
+        .and_then(|h| h.absolute_height())
+        .map_or(14, |v| v.get());
+
     let stripes = cli.flag.stripes();
-    let stripe_height = HEIGHT / stripes.len();
+    let stripe_height = height / stripes.len();
 
     for stripe_color in stripes {
         let bg = stripe_color.bg();
-        let line = format!("{bg}{}{RESET}\n", " ".repeat(WIDTH));
+        let line = format!("{bg}{}{RESET}\n", " ".repeat(width));
         let stripe = line.repeat(stripe_height);
         print!("{stripe}");
     }
